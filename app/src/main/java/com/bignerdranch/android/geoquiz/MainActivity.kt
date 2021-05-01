9cbf856ec090546e9aff88e7ca8512448d215672
Michael Lu <yeskky@gmail.com>
2021-05-01 20:20:16 +0800

    用ViewModel存储数据，这样当配置改变发生时，可以保留数据

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,11 +2,16 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,15 +21,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
-            Question(R.string.question_australia, true),
-            Question(R.string.question_oceans, true),
-            Question(R.string.question_mideast, false),
-            Question(R.string.question_africa, false),
-            Question(R.string.question_americas, true),
-            Question(R.string.question_asia, true))
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
 
-    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,64 +38,67 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
-            updateAnswerButtonState()
+//            updateAnswerButtonState()
         }
 
         falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
-            updateAnswerButtonState()
+//            updateAnswerButtonState()
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
         previousButton.setOnClickListener {
-            currentIndex = if (currentIndex == 0) questionBank.size - 1
-            else (currentIndex - 1) % questionBank.size
+//            currentIndex = if (currentIndex == 0) questionBank.size - 1
+//            else (currentIndex - 1) % questionBank.size
             updateQuestion()
         }
 
         questionTextView.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
         updateQuestion()
     }
 
     private fun updateQuestion() {
-        questionTextView.setText(questionBank[currentIndex].textResId)
-        updateAnswerButtonState()
-    }
-
-    private fun getScoreRating() {
-        val totalScore = questionBank.sumBy { it.score }
-        Toast.makeText(this, "总得分：${totalScore * 100 / questionBank.size}%", Toast.LENGTH_SHORT).show()
+        questionTextView.setText(quizViewModel.currentQuestionText)
+//        updateAnswerButtonState()
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
-        questionBank[currentIndex].hasBeenAnswered = true
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+//        questionBank[currentIndex].hasBeenAnswered = true
         val isAnswerCorrect = userAnswer == correctAnswer
 
         val messageResId = if (isAnswerCorrect) R.string.correct_toast else R.string.incorrect_toast
-        questionBank[currentIndex].score = if (isAnswerCorrect) 1 else 0
+//        questionBank[currentIndex].score = if (isAnswerCorrect) 1 else 0
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
-        if (currentIndex == questionBank.size - 1) getScoreRating()
+//        if (currentIndex == questionBank.size - 1) getScoreRating()
     }
 
-    /**
-     * 3.8挑战练习：用户答完某道题，就林掉那道题对应的按钮，用户一题多答
-     */
-    private fun updateAnswerButtonState() {
-        if (questionBank[currentIndex].hasBeenAnswered) {
-            trueButton.isEnabled = false
-            falseButton.isEnabled = false
-        } else {
-            trueButton.isEnabled = true
-            falseButton.isEnabled = true
-        }
-    }
+//    private fun getScoreRating() {
+//        val totalScore = questionBank.sumBy { it.score }
+//        Toast.makeText(this, "总得分：${totalScore * 100 / questionBank.size}%", Toast.LENGTH_SHORT)
+//            .show()
+//    }
+//
+
+//
+//    /**
+//     * 3.8挑战练习：用户答完某道题，就林掉那道题对应的按钮，用户一题多答
+//     */
+//    private fun updateAnswerButtonState() {
+//        if (questionBank[currentIndex].hasBeenAnswered) {
+//            trueButton.isEnabled = false
+//            falseButton.isEnabled = false
+//        } else {
+//            trueButton.isEnabled = true
+//            falseButton.isEnabled = true
+//        }
+//    }
 }
